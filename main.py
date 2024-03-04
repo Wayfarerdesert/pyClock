3f9fc56589fd451e5f93f37186cb25db177d0253
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-03-04 23:59:21 +0100

    pyClock functional, styles to be updated

--- main.py
+++ main.py
@@ -9,17 +9,26 @@ def update():
     day_string = strftime("%A")
     day_label.config(text=day_string)
 
-    time_label.after(1000, update)
+    date_string = strftime("%B %d %Y")
+    date_label.config(text=date_string)
+
+    window.after(1000, update)
 
 
 window = Tk()
+window.title("Simple Clock")
 window.resizable(False, False)
+# Set window size
+window.geometry("400x200")
 
 time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
-time_label.pack()
+time_label.pack(fill="x") # Make the time label occupy the whole width
 
 day_label = Label(window, font=("Arial", 25))
-day_label.pack()
+day_label.pack(side='bottom', anchor='center')
+
+date_label = Label(window, font=("Arial", 25))
+date_label.pack(side='bottom', anchor='center')
 
 update()
 
